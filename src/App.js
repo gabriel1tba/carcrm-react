d445f098bc44e789fd8615ae9735ea27e739e721
Gabriel Ferreira <gabriel.ferreira.itba@gmail.com>
2021-02-02 08:38:59 -0300

    feat: instalação e configuração do Material-UI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+// Dependencies
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+
+// Material-UI Theme Config
+import themeConfig from './styles/themeConfig';
+const theme = createMuiTheme(themeConfig);
+
 // Component
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <h1>Olá</h1>
-    </>
+    </ThemeProvider>
   );
 }
 
---
+++ src/styles/themeConfig.js
@@ -0,0 +1,22 @@
+const theme = {
+  palette: {
+    primary: {
+      main: '#2196f3',
+    },
+  },
+
+  props: {
+    MuiTextField: {
+      variant: 'outlined',
+      fullWidth: true,
+    },
+
+    MuiSelect: {
+      variant: 'outlined',
+      fullWidth: true,
+    },
+  },
+};
+
+// Export module
+export default theme;
