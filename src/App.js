ee1f52022fd01e430705ee6a48399430072cf551
Gabriel Ferreira <gabriel.ferreira.itba@gmail.com>
2021-02-02 09:53:17 -0300

    feat: configurando rotas

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import Routes from './routes/routes';
+
 import themeUiConfig from './styles/themeUiConfig';
 const theme = createMuiTheme(themeUiConfig);
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <h1>Olá</h1>
+      <Routes />
     </ThemeProvider>
   );
 }
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,18 +1,24 @@
-import { Suspense, lazy } from 'react';
+import React, { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import { CircularProgress } from '@material-ui/core';
 
 function Routes() {
-  <Router>
-    <Suspense
-      fallback={
-        <div className="d-flex justify-content-center">
-          <CircularProgress />
-        </div>
-      }
-    ></Suspense>
-  </Router>;
+  return (
+    <Router>
+      <Suspense
+        fallback={
+          <div className="d-flex justify-content-center mt-5 pt-5">
+            <CircularProgress />
+          </div>
+        }
+      >
+        <Switch>
+          <Route path="/" exact component={() => <h1>Home</h1>} />
+        </Switch>
+      </Suspense>
+    </Router>
+  );
 }
 
 export default Routes;
