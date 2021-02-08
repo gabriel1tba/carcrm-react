572fa6f546ae97a5b2a0c0c5218332ac04709fb7
Gabriel Ferreira <gabriel.ferreira.itba@gmail.com>
2021-02-08 17:49:55 -0300

    feat: adicionando navitem com menu dropdown

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,13 @@
 import { Link } from 'react-router-dom';
+import { MenuList, MenuItem } from '@material-ui/core';
+import {
+  FaCar,
+  FaUsers,
+  FaLaptop,
+  FaCreditCard,
+  FaWhatsapp,
+  FaSignOutAlt,
+} from 'react-icons/fa';
 
 import logoImg from '../../assets/logo.png';
 
@@ -8,7 +17,7 @@ function Header() {
       {window.innerWidth < 577 ? (
         <div></div>
       ) : (
-        <nav class="header navbar navbar-expand-lg navbar-light bg-white">
+        <nav class="header navbar navbar-expand-lg navbar-light bg-white p-0">
           <div className="container">
             <Link className="navbar-brand" to="/">
               <img src={logoImg} alt="Logo Car CRM" height="40" />
@@ -16,7 +25,64 @@ function Header() {
 
             <ul className="navbar-nav">
               <li className="nav-item">
-                <Link to="/vehicles" className="nav-link">Veículos</Link>
+                <Link to="/vehicles" className="nav-link">
+                  <FaCar className="icon-lg mr-1" /> Veículos
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <button className="nav-link bg-white">
+                  <FaUsers className="icon-lg mr-1 " /> Proprietários
+                </button>
+              </li>
+
+              <li className="nav-item dropdown">
+                <Link
+                  to="#"
+                  className="nav-link dropdown-toggle"
+                  data-toggle="dropdown"
+                >
+                  <FaLaptop className="icon-lg mr-1" /> Site
+                </Link>
+                <MenuList className="dropdown-menu">
+                  <MenuItem className="dropdown-item">
+                    Otimização para o Google
+                  </MenuItem>
+                  <MenuItem className="dropdown-item">
+                    Unidades e Telefone
+                  </MenuItem>
+                  <MenuItem className="dropdown-item">Minha Logo</MenuItem>
+                  <MenuItem className="dropdown-item">Domínio</MenuItem>
+                  <MenuItem className="dropdown-item">Configurações</MenuItem>
+                </MenuList>
+              </li>
+
+              <li className="nav-item dropdown">
+                <Link
+                  to="#"
+                  className="nav-link dropdown-toggle"
+                  data-toggle="dropdown"
+                >
+                  <FaCreditCard className="icon-lg mr-1" /> Financeiro
+                </Link>
+                <MenuList className="dropdown-menu">
+                  <MenuItem className="dropdown-item">Plano</MenuItem>
+                  <MenuItem className="dropdown-item">
+                    Minhas Transações
+                  </MenuItem>
+                </MenuList>
+              </li>
+
+              <li className="nav-item">
+                <Link to="#" className="nav-link">
+                  <FaWhatsapp className="icon-lg mr-1" /> Ajuda
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link to="#" className="nav-link">
+                  <FaSignOutAlt className="icon-lg mr-1" /> Sair
+                </Link>
               </li>
             </ul>
           </div>
