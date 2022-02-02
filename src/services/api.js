d7659908fb0d725f88b77130a0d6465323f97d7a
Gabriel Ferreira <gabriel.ferreira.itba@gmail.com>
2022-02-01 21:26:43 -0300

    feat: criando httpcliente para chamadas autenticadas

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,34 @@
 import axios from 'axios';
 
-const api = axios.create({
-  baseURL: 'http://localhost/',
+const baseURL = 'http://localhost/';
+const baseURLApi = 'http://localhost/api/';
+
+export const api = axios.create({
+  baseURL: baseURL,
 });
 
-export default api;
+export const apiAuth = axios.create({
+  baseURL: baseURLApi,
+});
 
-// baseURL: 'http://localhost/' => Root
-// baseURL: 'http://localhost/api/' => API
+apiAuth.interceptors.request.use((config) => {
+  config.headers.Authorization = `Bearer ${localStorage.getItem(
+    'access_token'
+  )}`;
+
+  return config;
+});
+
+apiAuth.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error?.response.status === 401) {
+      localStorage.removeItem('access_token');
+      window.location.href = 'login';
+    }
+
+    return Promise.reject(error);
+  }
+);
