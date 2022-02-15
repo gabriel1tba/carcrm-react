e53ce83a47df4e4a78d5f403e7af932f60e534ed
Gabriel Ferreira <gabriel.ferreira.itba@gmail.com>
2022-02-15 10:20:49 -0300

    feat: página de listagem de owners

---
+++ src/pages/Owners/index.js
@@ -0,0 +1,58 @@
+import { MdKeyboardBackspace } from 'react-icons/md';
+import { useDispatch, useSelector } from 'react-redux';
+import { FaPlus } from 'react-icons/fa';
+import {
+  AppBar,
+  Fab,
+  IconButton,
+  Toolbar,
+  Typography,
+} from '@material-ui/core';
+
+import { toggleScreen1, toggleScreen2 } from '../../store/actions/navigation';
+
+const Owners = () => {
+  const dispatch = useDispatch();
+
+  const handleCreate = () => {
+    dispatch(
+      toggleScreen2({
+        open: true,
+        type: 'owner-edit',
+        props: {},
+      })
+    );
+  };
+
+  return (
+    <>
+      <AppBar position="absolute">
+        <Toolbar>
+          <IconButton
+            onClick={() => dispatch(toggleScreen1({ open: false }))}
+            edge="start"
+            color="inherit"
+          >
+            <MdKeyboardBackspace />
+          </IconButton>
+
+          <Typography variant="h6" color="inherit">
+            Proprietários
+          </Typography>
+        </Toolbar>
+      </AppBar>
+
+      <div id="scroll" className="scroll">
+        <Fab
+          onClick={() => handleCreate()}
+          className="fab-bottom-right mr-3 mb-3"
+          color="primary"
+        >
+          <FaPlus />
+        </Fab>
+      </div>
+    </>
+  );
+};
+
+export default Owners;
