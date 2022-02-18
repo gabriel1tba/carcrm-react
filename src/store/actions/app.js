61dfe84617f2f789d93c830cfa538b3e8621d048
Gabriel Ferreira <gabriel.ferreira.itba@gmail.com>
2022-02-18 15:27:47 -0300

    feat: criando função validateSubdomain

--- src/store/actions/app.js
+++ src/store/actions/app.js
@@ -1,5 +1,6 @@
 import { showNotify } from './notify';
 import { showLoading } from './loading';
+import { showAlert } from './alert';
 
 import { apiAuth } from '../../services/api';
 
@@ -110,4 +111,43 @@ export const destroyLogo = (id) => async (dispatch) => {
   } catch (error) {
     console.log(error);
   }
+};
+
+// VALIDATE SUBDOMAIN
+export const validateSubdomain = (value) => (dispatch) => {
+  value = value.toLowerCase();
+  if (value.search(' ') >= 0) {
+    value = value.replace(' ', '');
+    dispatch(
+      showAlert({
+        open: true,
+        msg: 'O endereço do seu site não pode conter espaços em branco',
+        class: 'error',
+      })
+    );
+  }
+
+  if (value.search('www') >= 0) {
+    value = value.replace('www', '');
+    dispatch(
+      showAlert({
+        open: true,
+        msg: 'Digite o endereço do seu site sem o www',
+        class: 'error',
+      })
+    );
+  }
+
+  if (value.search('.com') >= 0) {
+    value = value.replace('.com', '');
+    dispatch(
+      showAlert({
+        open: true,
+        msg: 'Para utilizar domínio .com clique no botão abaixo "USAR MEU DOMÍNIO"',
+        class: 'error',
+      })
+    );
+  }
+
+  return value.normalize('NFD').replace(/([\u0300-\u036f]|[^0-9a-zA-Z.])/g, '');
 };
