988666dd2278dad0a779f784f1305ed1e67c9c2d
Gabriel Ferreira <gabriel.ferreira.itba@gmail.com>
2022-02-19 11:58:38 -0300

    feat: criando componente Pay

--- src/components/Pay/index.js
+++ src/components/Pay/index.js
@@ -1,5 +1,36 @@
+import { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import Header from '../Header';
+import Account from './components/Account';
+import Plans from './components/Plans';
+import Cart from './components/Cart';
+
 const Pay = () => {
-  return <h1>Pay</h1>;
+  const dispatch = useDispatch();
+  const app = useSelector((state) => state.appReducer.app);
+  const plan = useSelector((state) => state.payReducer.plan);
+  const [title, setTitle] = useState();
+
+  useEffect(() => {
+    if (app.status === 0 || app.status === 1) {
+      setTitle(Object.keys(plan).length === 0 ? 'Planos' : 'Pagamento');
+    }
+    if (app.status === 2) {
+      setTitle('Minha conta');
+    }
+  }, [app, plan]);
+
+  return (
+    <>
+      <Header title={title} />
+      {(app.status === 0 || app.status === 1) && (
+        <>{Object.keys(plan).length === 0 ? <Plans /> : <Cart />}</>
+      )}
+
+      {app.status === 2 && <Account />}
+    </>
+  );
 };
 
 export default Pay;
