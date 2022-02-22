0419f3c0b4757001589e69386e37e01e30a7fa8d
Gabriel Ferreira <gabriel.ferreira.itba@gmail.com>
2022-02-22 10:50:08 -0300

    feat: finalizando Transaction page

--- src/pages/Transactions/index.js
+++ src/pages/Transactions/index.js
@@ -2,6 +2,15 @@
 import { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { CircularProgress } from '@material-ui/core';
+import { FcOpenedFolder } from 'react-icons/fc';
+import { Link } from 'react-router-dom';
+import {
+  MdAccessTime,
+  MdCached,
+  MdCheckCircle,
+  MdDone,
+  MdWarning,
+} from 'react-icons/md';
 
 import Header from '../../components/Header';
 
@@ -69,9 +78,77 @@ const Transactions = () => {
           </div>
         ) : (
           <>
-            <div className="mb-4">
+            <div className="d-flex mb-4">
               <h3 className="font-weight-normal">Transações</h3>
+              <div className="ml-2">
+                <span className="badge badge-secondary">
+                  {transactions.total}
+                </span>
+              </div>
             </div>
+
+            {transactions.data.length < 1 && (
+              <div className="text-center mt-5 pt-5 mb-5 pb-5">
+                <FcOpenedFolder size="70" />
+                <h6 className="mt-4 text-muted">
+                  Nenhuma transação encontrada
+                </h6>
+              </div>
+            )}
+
+            {transactions.data.map((item, index) => (
+              <div key={index} className="card mb-3">
+                <div className="card-header d-flex justify-content-between">
+                  <h5 className="mb-md-0">Recebimento</h5>
+                  <h4 className="mb-md-0">
+                    {new Intl.NumberFormat('pt-BR', {
+                      style: 'currency',
+                      currency: 'BRL',
+                    }).format(item.transaction_amount)}
+                  </h4>
+                </div>
+
+                <div className="card-body">
+                  <Link to={`/transactions/${item.transaction_id}`}>
+                    <div className="d-flex">
+                      <div className="mr-4">
+                        {(item.status === 'pending' ||
+                          item.status === 'in_process') && (
+                          <MdAccessTime className={`h1 ${item.status}`} />
+                        )}
+                        {item.status === 'approved' && (
+                          <MdCheckCircle className={`h1 ${item.status}`} />
+                        )}
+                        {item.status === 'authorized' && (
+                          <MdDone className={`h1 ${item.status}`} />
+                        )}
+                        {(item.status === 'in_mediation' ||
+                          item.status === 'cancelled' ||
+                          item.status === 'rejected') && (
+                          <MdWarning className={`h1 ${item.status}`} />
+                        )}
+                        {(item.status === 'refunded' ||
+                          item.status === 'charged_back') && (
+                          <MdCached className={`h1 ${item.status}`} />
+                        )}
+                      </div>
+                      <div>
+                        <h5>{item.description}</h5>
+                        <span className={`font-weight-bold ${item.status}`}>
+                          {item.status_pt}
+                        </span>
+                      </div>
+                    </div>
+                  </Link>
+                </div>
+              </div>
+            ))}
+
+            {isLoadMore && (
+              <div className="text-center card-body">
+                <CircularProgress />
+              </div>
+            )}
           </>
         )}
       </div>
